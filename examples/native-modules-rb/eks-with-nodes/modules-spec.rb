43dbe99dc85a8ae893551155cc3030b56dad4b08
Slava Mogilevsky <triggerwoods91@gmail.com>
2024-04-13 22:51:11 +0200

    no day without pushing to main

--- examples/native-modules-rb/eks-with-nodes/modules-spec.rb
+++ examples/native-modules-rb/eks-with-nodes/modules-spec.rb
@@ -1,6 +1,20 @@
 require 'module-spec'
 
-cluster =  one :aws_eks_cluster
+cluster = ModuleSpec::one :aws_eks_cluster
+
+tls_certificate = one :tls_certificate, readonly: true do |tc, root|
+    tc.url root.identity[0].oidc[0].issuer
+end
+
+connect_providers = cluster.has_many :aws_iam_openid_connect_provider do |providers, root|
+    providers.max_count 1
+
+    providers.where_equal do |pr|
+        pr.url root.identity[0].oidc[0].issuer
+        pr.client_id_list ["sts.amazonaws.com"]
+        pr.thumbprint_list [tls_certificate.certificates[0].sha1_fingerprint]
+    end
+end
 
 connect_providers = many :aws_iam_openid_connect_provider do |c|
     max_count 1
@@ -14,9 +28,7 @@ addons = many :aws_eks_addon do |a|
     a.cluster_name cluster.id
 end
 
-tls_certificate = one :tls_certificate, readonly: true do |tc|
-    tc.url identity[0].oidc[0].issuer
-end
+
 
 templates = many :aws_launch_template do |lt|
     where do
