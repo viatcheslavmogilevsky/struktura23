226be38b179402973c5f06ac77158bfa12c2cd54
Slava Mogilevsky <triggerwoods91@gmail.com>
2024-04-25 07:17:10 +0200

    module magic works

--- lib/struktura23/base-spec.rb
+++ lib/struktura23/base-spec.rb
@@ -1,12 +1,23 @@
 module Struktura23
-  class BaseSpec
-    @@entrypoint = {}
-
-    class << self
-      def has_one rsr
-        @@entrypoint[:root] = rsr
-      end
+  module Basic
+    def entrypoint
+      @entrypoint ||= {}
     end
+
+    def has_one rsr
+      entrypoint[:root] = rsr
+    end
+  end
+
+
+  class BaseSpec
+    extend Basic
+    # class_attribute :entrypoint
+
+    # class << self
+    #   def has_one rsr
+    #   end
+    # end
 
   end
 end
