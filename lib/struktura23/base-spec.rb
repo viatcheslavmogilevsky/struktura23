f90e2dbd24e8e7ee960fb762a7859c8d3e530a0e
Slava Mogilevsky <triggerwoods91@gmail.com>
2024-04-26 06:30:57 +0200

    entrypointing

--- lib/struktura23/base-spec.rb
+++ lib/struktura23/base-spec.rb
@@ -1,23 +1,24 @@
 module Struktura23
-  module Basic
+  module Entrypoint
     def entrypoint
-      @entrypoint ||= {}
+      @entrypoint ||= []
     end
 
-    def has_one rsr
-      entrypoint[:root] = rsr
+    def is_repeatable?
+      entrypoint.length == 1
+    end
+
+    def entrypoint?
+      entrypoint.length > 0
+    end
+
+    def has_one resource
+      entrypoint << resource
     end
   end
 
 
   class BaseSpec
-    extend Basic
-    # class_attribute :entrypoint
-
-    # class << self
-    #   def has_one rsr
-    #   end
-    # end
-
+    extend Entrypoint
   end
 end
