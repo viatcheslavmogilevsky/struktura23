a87e82d28469a7bfad0a633e069c8bd2c2fd21d2
Slava Mogilevsky <triggerwoods91@gmail.com>
2024-09-27 18:01:15 +0200

    spec class is hcl-block generator

--- lib/struktura23/module-spec.rb
+++ lib/struktura23/module-spec.rb
@@ -4,5 +4,14 @@ module Struktura23
       raise "Root node is already specified" if !!@root
       @root = Node::Base.new(nil, Connection::Plural.new(nil))
     end
+
+    def self.nodes
+      @root.all_connected_nodes
+    end
+
+    # Stub
+    def self.hcl_blocks
+      []
+    end
   end
 end
