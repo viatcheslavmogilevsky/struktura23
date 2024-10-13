06e1850905680dbbd93ec7eaaa71159fa74b9bd1
Slava Mogilevsky <triggerwoods91@gmail.com>
2024-10-13 18:52:52 +0200

    real example: eks module: merge eks-core and eks-compute

--- examples/eks-stack/modules/eks/main.tf
+++ examples/eks-stack/modules/eks/main.tf
@@ -37,3 +37,66 @@ resource "aws_eks_addon" "this" {
     aws_iam_openid_connect_provider.this,
   ]
 }
+
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.id
+  node_group_name = var.eks_node_group_name
+  node_role_arn   = var.eks_node_role_arn
+  subnet_ids      = var.eks_node_group_subnet_ids
+  labels          = var.eks_node_group_labels
+  capacity_type   = var.eks_node_group_capacity_type
+
+
+  scaling_config {
+    desired_size = var.eks_node_group_desired_size
+    max_size     = var.eks_node_group_max_size
+    min_size     = var.eks_node_group_min_size
+  }
+
+  launch_template {
+    name    = aws_launch_template.this.name
+    version = aws_launch_template.this.latest_version
+  }
+
+  lifecycle {
+    ignore_changes = [scaling_config[0].desired_size]
+  }
+}
+
+data "aws_ami" "this" {
+  most_recent = true
+  name_regex  = "amazon-eks-arm64-node-${aws_eks_cluster.this.version}-v*"
+
+  owners = ["amazon"]
+}
+
+resource "aws_launch_template" "this" {
+  name_prefix            = var.eks_node_group_launch_template_name
+  ebs_optimized          = "true"
+  image_id               = data.aws_ami.this.image_id
+  instance_type          = var.eks_node_group_instance_type
+  key_name               = var.eks_node_group_ssh_key
+  vpc_security_group_ids = var.eks_node_group_security_group_ids
+
+  tag_specifications {
+    resource_type = "instance"
+    tags          = var.eks_node_group_tags
+  }
+
+  lifecycle {
+    ignore_changes = [
+      tag_specifications[0].tags["Owner"]
+    ]
+  }
+
+  user_data = base64encode(<<-EOT
+    #!/bin/bash -xe
+
+    # Bootstrap and join the cluster
+    /etc/eks/bootstrap.sh --b64-cluster-ca '${aws_eks_cluster.this.certificate_authority[0].data}' --apiserver-endpoint '${aws_eks_cluster.this.endpoint}' '${aws_eks_cluster.this.id}'
+
+    # Allow user supplied userdata code
+    echo "Node is up..."
+  EOT
+  )
+}
--- examples/eks-stack/modules/eks/variables.tf
+++ examples/eks-stack/modules/eks/variables.tf
@@ -6,7 +6,7 @@ variable "eks_cluster_name" {
 variable "eks_cluster_version" {
   description = "Desired k8s version"
   type        = string
-  default     = "1.27"
+  default     = "1.29"
 }
 
 variable "eks_role_arn" {
@@ -53,3 +53,69 @@ variable "eks_security_group_ids" {
   type        = list(string)
   default     = null
 }
+
+variable "eks_node_group_name" {
+  description = "The name of the node group"
+  type        = string
+}
+
+variable "eks_node_role_arn" {
+  description = "Role ARN of the EKS nodes"
+  type        = string
+}
+
+variable "eks_node_group_subnet_ids" {
+  description = "Subnet IDs of the node groups"
+  type        = list(string)
+}
+
+variable "eks_node_group_desired_size" {
+  description = "Desired size of the eks node group"
+  type        = number
+}
+
+variable "eks_node_group_max_size" {
+  description = "Max size of the node group"
+  type        = number
+}
+
+variable "eks_node_group_capacity_type" {
+  description = "Type of capacity associated with the EKS Node Group."
+  default     = "ON_DEMAND"
+  type        = string
+}
+variable "eks_node_group_min_size" {
+  description = "Minimum size of the node group"
+  type        = number
+}
+
+variable "eks_node_group_instance_type" {
+  description = "Instance types of the node group (t3.medium, c5,xlarge etc.)"
+  type        = string
+}
+
+variable "eks_node_group_labels" {
+  description = "Labels of the node group"
+  type        = map(any)
+}
+
+variable "eks_node_group_ssh_key" {
+  description = "SSH key for the nodes"
+  type        = string
+}
+
+variable "eks_node_group_security_group_ids" {
+  description = "Security groups for the node groups"
+  type        = list(string)
+}
+
+variable "eks_node_group_launch_template_name" {
+  description = "Launch template name"
+  type        = string
+}
+
+variable "eks_node_group_tags" {
+  description = "Tags for the node groups"
+  type        = map(any)
+}
+
