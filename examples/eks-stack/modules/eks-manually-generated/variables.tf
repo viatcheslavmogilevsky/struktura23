bcc06558e90967985d3595898d856d8abb172b8a
Slava Mogilevsky <triggerwoods91@gmail.com>
2024-11-30 10:15:42 +0100

    WIP: full module example

--- examples/eks-stack/modules/eks-manually-generated/variables.tf
+++ examples/eks-stack/modules/eks-manually-generated/variables.tf
@@ -200,7 +200,6 @@ variable "eks_node_groups" {
 variable "eks_node_groups_common" {
   type = object({
     enabled = optional(bool, true)
-    launch_template_key = optional(string)
 
     node_role_arn = optional(string)
     scaling_config = optional(object({
@@ -238,6 +237,88 @@ variable "eks_node_groups_common" {
       max_unavailable_percentage = optional(number)
     }))
     version = optional(string)
+  })
+
+  default = {
+    enabled = true
+  }
+}
+
+variable "launch_templates" {
+  type = map(object({
+    enabled = optional(bool, true)
+
+    block_device_mappings = optional(list(object({
+      device_name = optional(string)
+      ebs = optional(object({
+        delete_on_termination = optional(bool) # nullable.TypeNullableBool
+        encrypted = optional(bool) # nullable.TypeNullableBool
+        iops = optional(number)
+        kms_key_id = optional(string)
+        snapshot_id = optional(string)
+        throughput = optional(number)
+        volume_size = optional(number)
+        volume_type = optional(string)
+      }))
+      no_device = optional(string)
+      virtual_name = optional(string)
+    })))
+
+    capacity_reservation_specification = optional(object({
+      capacity_reservation_preference = optional(string)
+      capacity_reservation_target = optional(object({
+        capacity_reservation_id = optional(string)
+        capacity_reservation_resource_group_arn = optional(string)
+      }))
+    }))
+
+    cpu_options = optional(object({
+      amd_sev_snp = optional(string)
+      core_count = optional(number)
+      threads_per_core = optional(number)
+    }))
+
+    credit_specification = optional(object({
+      cpu_credits = optional(string)
+    }))
+
+    default_version = optional(number)
+    description = optional(string)
+    disable_api_stop = optional(bool)
+    disable_api_termination = optional(bool)
+    ebs_optimized = optional(bool) # nullable.TypeNullableBool
+
+    elastic_gpu_specifications = optional(list(object({
+      type = string
+    })))
+
+    elastic_inference_accelerator = optional(object({
+      type = string
+    }))
+
+    enclave_options = optional(object({
+      enabled = optional(bool)
+    }))
+
+    hibernation_options = optional(object({
+      configured = bool
+    }))
+
+    iam_instance_profile = optional(object({
+      arn = optional(string)
+      name = optional(string)
+    }))
+
+    image_id = optional(string)
+  }))
+
+  default = {}
+}
+
+variable "launch_templates_common" {
+  type = object({
+    enabled = optional(bool, true)
+
   })
 
   default = {
