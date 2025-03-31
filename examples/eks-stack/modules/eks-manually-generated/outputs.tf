c69ac4e47b0295553e3fe71d74a0d71aea385b5a
Slava Mogilevsky <triggerwoods91@gmail.com>
2025-04-01 01:24:04 +0200

    WIP: eks-manually-generated module

--- examples/eks-stack/modules/eks-manually-generated/outputs.tf
+++ examples/eks-stack/modules/eks-manually-generated/outputs.tf
@@ -72,17 +72,17 @@ output "certificate_certificates" {
 
 # iam_openid_connect_provider
 
-## because it's enforced:
-output "iam_openid_connect_provider_thumbprint_list" {
-  value = one(aws_iam_openid_connect_provider.this[*].thumbprint_list)
-}
-
 output "iam_openid_connect_provider_arn" {
   value = one(aws_iam_openid_connect_provider.this[*].arn)
 }
 
 output "iam_openid_connect_provider_tags_all" {
   value = one(aws_iam_openid_connect_provider.this[*].tags_all)
+}
+
+## because it's enforced:
+output "iam_openid_connect_provider_thumbprint_list" {
+  value = one(aws_iam_openid_connect_provider.this[*].thumbprint_list)
 }
 
 output "iam_openid_connect_provider_id" {
@@ -93,10 +93,6 @@ output "iam_openid_connect_provider_id" {
 
 output "eks_addon_arn_map" {
   value = { for k, v in var.eks_addons : k => aws_eks_addon.this[k].arn if v.enabled && k != "_common" }
-}
-
-output "eks_addon_id_map" {
-  value = { for k, v in var.eks_addons : k => lookup(aws_eks_addon.this[k], "id", null) if v.enabled && k != "_common" }
 }
 
 output "eks_addon_created_at_map" {
@@ -119,17 +115,138 @@ output "eks_addon_configuration_values_map" {
   value = { for k, v in var.eks_addons : k => aws_eks_addon.this[k].configuration_values if v.enabled && k != "_common" }
 }
 
+output "eks_addon_id_map" {
+  value = { for k, v in var.eks_addons : k => lookup(aws_eks_addon.this[k], "id", null) if v.enabled && k != "_common" }
+}
 
-# WIP: node_groups
+# eks_node_group
 
-output "eks_nodegroup_arn_map" {
+output "eks_node_group_ami_type_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].ami_type if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_arn_map" {
   value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].arn if v.enabled && k != "_common" }
 }
 
-# WIP: launch_templates
+output "eks_node_group_capacity_type_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].capacity_type if v.enabled && k != "_common" }
+}
 
-# TBD
+output "eks_node_group_disk_size_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].disk_size if v.enabled && k != "_common" }
+}
 
-# WIP: AMIs
+output "eks_node_group_instance_types_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].instance_types if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_launch_template_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].launch_template if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_node_group_name_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].node_group_name if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_node_group_name_prefix_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].node_group_name_prefix if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_release_version_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].release_version if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_resources_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].resources if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_status_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].status if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_tags_all_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].tags_all if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_update_config_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].update_config if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_version_map" {
+  value = { for k, v in var.eks_node_groups : k => aws_eks_node_group.this[k].version if v.enabled && k != "_common" }
+}
+
+output "eks_node_group_id_map" {
+  value = { for k, v in var.eks_node_groups : k => lookup(aws_eks_node_group.this[k], "id", null) if v.enabled && k != "_common" }
+}
+
+# launch_template
+
+output "launch_template_arn_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].arn if v.enabled && k != "_common" }
+}
+
+output "launch_template_block_device_mappings_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].block_device_mappings if v.enabled && k != "_common" }
+}
+
+output "launch_template_default_version_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].default_version if v.enabled && k != "_common" }
+}
+
+output "launch_template_instance_market_options_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].instance_market_options if v.enabled && k != "_common" }
+}
+
+output "launch_template_latest_version_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].latest_version if v.enabled && k != "_common" }
+}
+
+output "launch_template_metadata_options_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].metadata_options if v.enabled && k != "_common" }
+}
+
+output "launch_template_name_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].name if v.enabled && k != "_common" }
+}
+
+output "launch_template_name_prefix_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].name_prefix if v.enabled && k != "_common" }
+}
+
+output "launch_template_tags_all_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].tags_all if v.enabled && k != "_common" }
+}
+
+## because it's enforced:
+output "launch_template_image_id_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].image_id if v.enabled && k != "_common" }
+}
+
+## because it's enforced:
+output "launch_template_user_data_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].user_data if v.enabled && k != "_common" }
+}
+
+## because it's enforced:
+output "launch_template_tag_specifications_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].tag_specifications if v.enabled && k != "_common" }
+}
+
+## because it's enforced:
+output "launch_template_vpc_security_group_ids_map" {
+  value = { for k, v in var.launch_templates : k => aws_launch_template.this[k].vpc_security_group_ids if v.enabled && k != "_common" }
+}
+
+output "launch_template_id_map" {
+  value = { for k, v in var.launch_templates : k => lookup(aws_launch_template.this[k], "id", null) if v.enabled && k != "_common" }
+}
+
+# ami
+
+output "ami_architecture_map" {
+  value = { for k, v in local.merged_no_key_attrs["launch_templates"] : k =>  data.aws_ami.this[k].architecture if v.ami != null }
+}
 
 # TBD
